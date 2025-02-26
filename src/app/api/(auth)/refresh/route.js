c42a59067d5e755aa857b2fce2408e7e9962fca0
Nam <thanhnamak@gmail.com>
2025-02-26 14:14:13 +0700

    secure = false, allowing http

--- src/app/api/(auth)/refresh/route.js
+++ src/app/api/(auth)/refresh/route.js
@@ -43,7 +43,7 @@ export async function POST(req) {
 
         response.cookies.set("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
+            secure: false,
             sameSite: "strict",
             path: "/"
         });
--- src/app/api/(auth)/verify-otp/route.js
+++ src/app/api/(auth)/verify-otp/route.js
@@ -64,7 +64,7 @@ export async function POST(req) {
 
         response.cookies.set("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
+            secure: false,
             sameSite: "strict",
             path: "/"
         });
