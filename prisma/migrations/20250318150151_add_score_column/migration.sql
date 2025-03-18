dcfe35997b4719285523eaec2ae20a6678b9f531
Nam <thanhnamak@gmail.com>
2025-03-18 22:17:30 +0700

    some changes

---
+++ prisma/migrations/20250318150151_add_score_column/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `question_options` ADD COLUMN `score` INTEGER NULL;
---
+++ prisma/updatescore.ts
@@ -0,0 +1,23 @@
+import PrismaClient from "@prisma/client";
+
+interface QuestionScoreSeed {
+    question_option_id: number,
+    score: number,
+}
+
+const db = new PrismaClient();
+
+foreach (question_score as question_update_score) {
+
+}
+
+export const question_score = [
+    {
+        question_option_id: 1,
+        score: 1,
+    },
+]
+
+export const update_question = {
+
+}
