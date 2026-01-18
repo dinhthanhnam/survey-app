b748e49094e8b123cd4b58c597ec1a71f9d3b127
nghiathe <thenghia33@gmail.com>
2026-01-19 01:27:05 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import './globals.css'; // Đường dẫn tới file CSS của bạn
 
 export const metadata = {
-    title: 'Khảo sát chuyển đổi số - Ngân hàng Hợp tác',
-    description: 'Khảo sát chuyển đổi số - Ngân hàng Hợp tác',
+    title: 'Khảo sát chuyển đổi số - Ngân hàng phát triển Việt Nam',
+    description: 'Khảo sát chuyển đổi số - Ngân hàng phát triển Việt Nam',
 };
 
 export default function RootLayout({ children }) {
